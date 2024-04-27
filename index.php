070caf834015efebfe04166b85227e4cf4b9646a
Bipasha_Acharjee <118049787+Bipasha1005@users.noreply.github.com>
2024-04-27 11:23:35 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome to JR: Title Tag - <?php echo $_SESSION['Username']?></title>
+  <title>Welcome to JR rnd</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
